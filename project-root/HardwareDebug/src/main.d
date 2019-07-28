fbeb3eafcf459316a0a3f4c1c95de9912c1fa78c
Jay <nielcansino@gmail.com>
2019-07-28 18:39:00 +0900

    renamed project and fixed dev environment

--- project-root/HardwareDebug/src/main.d
+++ project-root/HardwareDebug/src/main.d
@@ -1,50 +1,50 @@
 src/main.obj\ src/main.d: ../src/main.c
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_smc_entry.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_smc_entry.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_cg_macrodriver.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_cg_macrodriver.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\platform.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\platform.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\./board/generic_rx65n/r_bsp.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\./board/generic_rx65n/r_bsp.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_config\r_bsp_config.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_config\r_bsp_config.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_config\r_bsp_interrupt_config.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_config\r_bsp_interrupt_config.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/register_access/iodefine.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/register_access/iodefine.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_info.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_info.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_locks.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_locks.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/locking.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/locking.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/cpu.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/cpu.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_init.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_init.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_interrupts.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_interrupts.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts_private.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts_private.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_startup.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_startup.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/hwsetup.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/hwsetup.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/lowsrc.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/lowsrc.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/vecttbl.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/vecttbl.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_smc_interrupt.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_smc_interrupt.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_glcdc_rx\r_glcdc_rx_if.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_glcdc_rx\r_glcdc_rx_if.h:
-src/main.obj\ src/main.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_pincfg\r_glcdc_rx_pinset.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_pincfg\r_glcdc_rx_pinset.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_smc_entry.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_smc_entry.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_cg_macrodriver.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_cg_macrodriver.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\platform.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\platform.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\./board/generic_rx65n/r_bsp.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\./board/generic_rx65n/r_bsp.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_config\r_bsp_config.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_config\r_bsp_config.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_config\r_bsp_interrupt_config.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_config\r_bsp_interrupt_config.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/register_access/iodefine.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/register_access/iodefine.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_info.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_info.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_locks.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_locks.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/locking.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/locking.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/cpu.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/cpu.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_init.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_init.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_interrupts.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_interrupts.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts_private.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts_private.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_startup.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_startup.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/hwsetup.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/hwsetup.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/lowsrc.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/lowsrc.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/vecttbl.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/vecttbl.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_smc_interrupt.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_smc_interrupt.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_glcdc_rx\r_glcdc_rx_if.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_glcdc_rx\r_glcdc_rx_if.h:
+src/main.obj\ src/main.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_pincfg\r_glcdc_rx_pinset.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_pincfg\r_glcdc_rx_pinset.h:
 src/main.obj\ src/main.d: ../src/r_image_config_RX65N_Envision.h
 ../src/r_image_config_RX65N_Envision.h:
 src/main.obj\ src/main.d: ../src/r_lcd_timing_RX65N_Envision.h
--- project-root/HardwareDebug/src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d
+++ project-root/HardwareDebug/src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d
@@ -1,41 +1,41 @@
 src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: ../src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.c
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\platform.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\platform.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\./board/generic_rx65n/r_bsp.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\./board/generic_rx65n/r_bsp.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_config\r_bsp_config.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_config\r_bsp_config.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_config\r_bsp_interrupt_config.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_config\r_bsp_interrupt_config.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/register_access/iodefine.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/register_access/iodefine.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_info.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_info.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_locks.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_locks.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/locking.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/locking.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/cpu.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/cpu.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_init.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_init.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_interrupts.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_interrupts.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts_private.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts_private.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_startup.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\mcu/rx65n/mcu_startup.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/hwsetup.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/hwsetup.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/lowsrc.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/lowsrc.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/vecttbl.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\r_bsp\board/generic_rx65n/vecttbl.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_cg_macrodriver.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_cg_macrodriver.h:
-src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_smc_interrupt.h
-G:\renesasMCU\envision\an-r20an0487ej0100-qedisplayrx-100\workspace\QE_for_Display_sample_RX65N_Envision\src\smc_gen\general\r_smc_interrupt.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\platform.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\platform.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\./board/generic_rx65n/r_bsp.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\./board/generic_rx65n/r_bsp.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_config\r_bsp_config.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_config\r_bsp_config.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_config\r_bsp_interrupt_config.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_config\r_bsp_interrupt_config.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/register_access/iodefine.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/register_access/iodefine.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_info.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_info.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_locks.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_locks.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/locking.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/locking.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/cpu.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/cpu.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_init.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_init.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_interrupts.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_interrupts.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts_private.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts_private.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_mapped_interrupts.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_startup.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\mcu/rx65n/mcu_startup.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/hwsetup.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/hwsetup.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/lowsrc.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/lowsrc.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/vecttbl.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\r_bsp\board/generic_rx65n/vecttbl.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_cg_macrodriver.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_cg_macrodriver.h:
+src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.obj\ src/smc_gen/r_bsp/board/generic_rx65n/hwsetup.d: G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_smc_interrupt.h
+G:\renesasMCU\rx-envision-x-lvgl\project-root\src\smc_gen\general\r_smc_interrupt.h:
